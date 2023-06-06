372be1e2e3271c30f14f5c88de5be766776d6fcf
Miguel Romero <migueljoestar@gmail.com>
2023-06-05 22:10:22 -0500

    Deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,6 @@ import react from "@astrojs/react";
 // https://astro.build/config
 export default defineConfig({
   integrations: [react()],
-  site: "miguelrlia117.github.io",
+  site: "https://miguelrlia117.github.io",
   base: "/MisteryLux-Landing",
 });
